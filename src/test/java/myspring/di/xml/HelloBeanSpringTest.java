181d8979840a0a0c689ae52aca72bee84684c33b
mysoyul <vega2k_@naver.com>
2024-05-07 14:20:37 +0900

    HelloBeanSpringTest update

--- src/test/java/myspring/di/xml/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/xml/HelloBeanSpringTest.java
@@ -1,11 +1,32 @@
 package myspring.di.xml;
 
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_di.xml")
 public class HelloBeanSpringTest {
+	@Autowired
+	Hello hello;
+	
+	@Autowired
+	@Qualifier("helloCons")
+	Hello hello2;
+	
+	@Resource(name = "helloCons")
+	Hello hello3;
+	
+	@Test
+	void helloBean() {
+		System.out.println(hello.sayHello());
+		System.out.println(hello2.sayHello());
+		System.out.println(hello3.sayHello());
+	}
 	
 }
