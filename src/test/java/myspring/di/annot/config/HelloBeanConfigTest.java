3be7a8579e682cac8d8439c2da0bc131ce0e3ab2
mysoyul <vega2k_@naver.com>
2024-05-07 16:33:56 +0900

    HelloBeanConfigTest update

---
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -0,0 +1,25 @@
+package myspring.di.annot.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import myspring.di.annot.HelloBean;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(
+		classes = HelloBeanConfig.class, 
+		loader=AnnotationConfigContextLoader.class)
+public class HelloBeanConfigTest {
+	@Autowired
+	HelloBean hello;
+	
+	@Test
+	void hello() {
+		System.out.println(hello.sayHello());
+	}
+	
+}
