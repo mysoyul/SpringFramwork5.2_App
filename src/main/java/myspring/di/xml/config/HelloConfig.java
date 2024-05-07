9aecee9a626b2c5989c27e40d688ed5291c93d82
mysoyul <vega2k_@naver.com>
2024-05-07 17:06:41 +0900

    HelloConfig Environment use

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,8 +1,10 @@
 package myspring.di.xml.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import myspring.di.xml.ConsolePrinter;
 import myspring.di.xml.Hello;
@@ -12,9 +14,13 @@ import myspring.di.xml.StringPrinter;
 @Configuration
 @PropertySource(value = "classpath:values.properties")
 public class HelloConfig {
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public Hello hello() {
 		Hello hello = new Hello();
+		hello.setName(env.getProperty("myName3"));
 		return hello;
 	}
 	
