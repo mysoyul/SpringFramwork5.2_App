bff67120273376e342eada95dd62e37fb0af1eea
mysoyul <vega2k_@naver.com>
2024-05-08 09:35:26 +0900

    spring_beans.xml add

--- src/test/java/myspring/user/DatabaseTest.java
+++ src/test/java/myspring/user/DatabaseTest.java
@@ -1,11 +1,37 @@
 package myspring.user;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_beans.xml")
 public class DatabaseTest {
-
+	@Autowired
+	DataSource dataSource;
+	
+	@Test
+	void conn() {
+		try {
+			Connection connection = dataSource.getConnection();
+			DatabaseMetaData metaData = connection.getMetaData();
+			System.out.println("DB Product Name = " + metaData.getDatabaseProductName());
+			System.out.println("DB Url = " + metaData.getURL());
+			System.out.println("DB Username = " + metaData.getUserName());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
