05a6f7ab6b95ccc07e33a61d6d70edb1cfe8b9da
mysoyul <vega2k_@naver.com>
2024-05-08 17:32:19 +0900

    UserRestController update

--- src/main/java/myspring/user/controller/RestfulUserController.java
+++ src/main/java/myspring/user/controller/RestfulUserController.java
@@ -2,8 +2,11 @@ package myspring.user.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,27 +18,32 @@ import myspring.user.vo.UserVO;
 import myspring.user.vo.xml.UserVOXML;
 
 @RestController
+@RequestMapping("/users")
 public class RestfulUserController {
-	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value="/users",
-			                 method=RequestMethod.GET,produces = { "application/json"})
+	//Constructor Injection
+	public RestfulUserController(UserService userService) {
+		System.out.println(this.getClass().getName() + " 생성자 호출됨!");
+		this.userService = userService;
+	}
+
+	//@RequestMapping(value="/users", method=RequestMethod.GET, produces = { "application/json"})
+	@GetMapping
 	public List<UserVO> getUserList() {
 		List<UserVO> userList = userService.getUserList();
 		return userList;
 	}
 	
-	@RequestMapping(value="/users/{id}",
-			                    method=RequestMethod.GET,produces = { "application/json"})
-	public UserVO getUser(@PathVariable String id) {
-		UserVO user = userService.getUser(id);
+	//@RequestMapping(value="/users/{id}", method=RequestMethod.GET,produces = { "application/json"})
+	@GetMapping("/{id}")
+	public UserVO getUser(@PathVariable("id") String userid) {
+		UserVO user = userService.getUser(userid);
 		return user;
 	}
 		
-	@RequestMapping(value="/users",
-			                 method=RequestMethod.POST,
-			                 headers = {"Content-type=application/json"})
+	//@RequestMapping(value="/users",method=RequestMethod.POST, headers = {"Content-type=application/json"})
+	@PostMapping
 	public Boolean insertUser(@RequestBody UserVO user) {
 		if (user != null) {
 			userService.insertUser(user);
@@ -45,11 +53,12 @@ public class RestfulUserController {
 		}
 	}
 	
-	@RequestMapping(value="/users",
-			                 method=RequestMethod.PUT,
-			                headers = {"Content-type=application/json"})
-	public Boolean updateUser(@RequestBody UserVO user) {
+	//@RequestMapping(value="/users",method=RequestMethod.PUT,headers = {"Content-type=application/json"})
+	@PatchMapping("/{userid}")
+	public Boolean updateUser(@PathVariable String userid, @RequestBody UserVO userDetail) {
+		UserVO user = userService.getUser(userid);
 		if (user != null) {
+			user.setCity(userDetail.getCity());
 			userService.updateUser(user);
 			return Boolean.TRUE;
 		} else {	
@@ -57,18 +66,19 @@ public class RestfulUserController {
 		}
 	}
 	
-	@RequestMapping(value="/users/{id}",
-			         method=RequestMethod.DELETE)
-	public Boolean deleteUser(@PathVariable String id) {
-		if (id != null) {
-			userService.deleteUser(id);
+	//@RequestMapping(value="/users/{id}", method=RequestMethod.DELETE)
+	@DeleteMapping("/{userid}")
+	public Boolean deleteUser(@PathVariable String userid) {
+		UserVO user = userService.getUser(userid);
+		if (user != null) {
+			userService.deleteUser(userid);
 			return Boolean.TRUE;
 		} else {		
 			return Boolean.FALSE;
 		}
 	}
 	
-	@RequestMapping(value="/usersXml", method=RequestMethod.GET, produces = { "application/xml"})
+	@RequestMapping(value="/xml", method=RequestMethod.GET, produces = { "application/xml"})
 	public UserVOXML getUserListXml() {
 		List<UserVO> list = userService.getUserList();
 		UserVOXML xml = new UserVOXML("success", list);
