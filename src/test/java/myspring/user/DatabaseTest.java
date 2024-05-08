3b612c08d6fa825c90954862ec68bd12cf919dca
mysoyul <vega2k_@naver.com>
2024-05-08 11:51:03 +0900

    DatabaseTest UserService

--- src/test/java/myspring/user/DatabaseTest.java
+++ src/test/java/myspring/user/DatabaseTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import myspring.user.dao.mapper.UserMapper;
+import myspring.user.service.UserService;
 import myspring.user.vo.UserVO;
 
 @ExtendWith(SpringExtension.class)
@@ -29,7 +30,16 @@ public class DatabaseTest {
 	@Autowired
 	UserMapper userMapper;
 	
+	@Autowired
+	UserService userService;
+	
 	@Test
+	void userSvc() {
+		UserVO user = userService.getUser("gildong");
+		System.out.println(user);
+	}
+	
+	@Test @Disabled
 	void userMapper() {
 		UserVO user = userMapper.selectUserById("gildong");
 		System.out.println(user);
